2e6fc97c25823442e56614c63ea634258c39f6b4
Chang Hoon <gelheart1011@gmail.com>
2022-04-12 17:38:34 +0900

    비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -6,6 +8,10 @@ const userSchema = mongoose.Schema({
     maxlength: 50,
   },
   email: {
+    type: String,
+    minlength: 5,
+  },
+  password: {
     type: String,
     minlength: 5,
   },
@@ -26,6 +32,24 @@ const userSchema = mongoose.Schema({
   },
 });
 
+// 저장하기전에 무엇을 한다
+userSchema.pre("save", function (next) {
+  var user = this;
+  // 비밀번호를 암호화한다
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
